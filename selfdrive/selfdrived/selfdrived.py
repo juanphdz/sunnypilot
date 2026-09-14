5b33181990ee220d6fb9fada2302ac7e5eac816c
juan <juanpablo_o@mac.lan>
2026-09-13 19:59:05 -0700

    Disable processNotRunning alert in simulation mode

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -368,7 +368,7 @@ class SelfdriveD(CruiseHelper):
       if not_running != self.not_running_prev:
         cloudlog.event("process_not_running", not_running=not_running, error=True)
       self.not_running_prev = not_running
-    if self.sm.recv_frame['managerState'] and (not_running - self.ignored_processes):
+    if self.sm.recv_frame['managerState'] and (not_running - self.ignored_processes) and not SIMULATION:
       self.events.add(EventName.processNotRunning)
     else:
       if not SIMULATION and not self.rk.lagging:
